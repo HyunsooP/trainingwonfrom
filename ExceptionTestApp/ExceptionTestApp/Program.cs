9ed9ee455fdeb7a710907b2a745e53bad4f19f32
hyunsooP <sorj03@naver.com>
2020-06-15 14:41:32 +0900

    예외처리

---
+++ ExceptionTestApp/ExceptionTestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 100, y = 0, value = 0;
+            value = x / y;
+            Console.Write($"value = {0}", value);
+        }
+    }
+}
