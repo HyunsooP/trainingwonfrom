09e587a0f5642378f7a6705b78ed7812caa1606e
hyunsooP <sorj03@naver.com>
2020-06-24 16:29:50 +0900

    오늘

---
+++ BookLentalShop/MainForm.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Windows.Forms;
+using MetroFramework;
+using MetroFramework.Forms;
+
+namespace BookRentalShop20
+{
+    public partial class MainForm : MetroForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 로그인 폼 연결 / 모달 생성
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            LoginForm loginForm = new LoginForm();
+            loginForm.ShowDialog();
+        }
+        /// <summary>
+        /// 폼 클로징 이벤트
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainForm_FormClosing(object sender, System.Windows.Forms.FormClosingEventArgs e)
+        {
+            if (MetroMessageBox.Show(this, "정말 종료하시겠습니까?", "종료",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                foreach (Form item in this.MdiChildren)
+                {
+                    item.Close();
+                }
+                e.Cancel = false;   //false는 취소가 되지 않았다는 뜻
+            }
+            else
+            {
+                e.Cancel = true;    //true가 취소 됬다는 뜻
+            }
+
+        }
+        /// <summary>
+        /// Dock 스타일
+        /// </summary>
+        /// <param name="form"></param>
+        /// <param name="strFormTitle"></param>
+        private void InitChildForm(Form form, string strFormTitle)
+        {
+            form.Text = strFormTitle;
+            form.Dock = DockStyle.Fill; //화면을 꽉 채움
+            form.MdiParent = this;  //
+            form.Show();
+            form.WindowState = FormWindowState.Maximized;
+        }
+        /// <summary>
+        /// 사용자 표시 라벨
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainForm_Activated(object sender, EventArgs e)
+        {
+            LblUserID.Text = Commons.LOGINUSERID;
+        }
+        /// <summary>
+        /// 툴스트립 불러오기
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MnuItemDivMng_Click(object sender, EventArgs e)                 // 구분코드 관리
+        {
+            DivForm form = new DivForm();
+            InitChildForm(form, "구분코드 관리");
+        }
+        private void 사용자관리UToolStripMenuItem_Click(object sender, EventArgs e)  // 사용자폼
+        {
+            UserForm form = new UserForm();
+            InitChildForm(form, "사용자 관리");
+        }
+        private void 회원관리MToolStripMenuItem_Click(object sender, EventArgs e)    // 회원관리폼
+        {
+            MemberForm form = new MemberForm();
+            InitChildForm(form, "회원 관리");
+        }
+        private void 책관리BToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BooksForm form = new BooksForm();
+            InitChildForm(form, "책 관리");
+        }   // 책관리폼
+
+        private void 대여관리RToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RentalForm form = new RentalForm();
+            InitChildForm(form, "대여 관리");
+        }
+    }
+}
---
+++ BookRentalShop/RentalTbl.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace BookRentalShop
+{
+    partial class RentalTbl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(334, 78);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(334, 156);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 23);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // RentalTbl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "RentalTbl";
+            this.Text = "RentalTbl";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
